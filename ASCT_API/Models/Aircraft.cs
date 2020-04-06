befab6bb385bd6f6731ff3487f7bbee65cbcdc9d
Batina Shikhalieva <bs35@uw.edu>
2020-04-06 07:00:48 +0300

    Connection to db from Model

--- ASCT_API/Models/Aircraft.cs
+++ ASCT_API/Models/Aircraft.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ASCT_Razor_Page.Models
 {
@@ -9,16 +10,22 @@ namespace ASCT_Razor_Page.Models
         [StringLength(6, MinimumLength = 6, ErrorMessage = "Invalid Tail Number.")]
         public string Tail_Number { get; set; }
 
+        [Column(TypeName = "nvarchar(100)")]
         public string LRU_Name { get; set; }
-
+        
+        [Column(TypeName = "nvarchar(100)")]
         public string LRU_Part_Number { get; set; }
-
+        
+        [Column(TypeName = "nvarchar(100)")]
         public string Software_Name { get; set; }
-
+        
+        [Column(TypeName = "nvarchar(100)")]
         public string Software_Part_Number { get; set; }
-
+        
+        [Column(TypeName = "nvarchar(100)")]
         public string EC_Number { get; set; }
 
+        [Column(TypeName = "nvarchar(100)")]
         public string Pre_Post_Condition { get; set; }
 
         public Aircraft()
